8c71d7b3225402089be46c653a200e902aefdd32
Medha <medha.nagesh@gmail.com>
2020-05-18 14:54:28 +0530

    Add helper to print gopher ascii

---
+++ src/gopher/pattern.go
@@ -0,0 +1,19 @@
+package gopher
+
+import "fmt"
+
+// Print gopher pattern
+func Print() {
+	fmt.Println("    `.-::::::-.`    ")
+	fmt.Println(".:-::::::::::::::-:.")
+	fmt.Println("`_:::    ::    :::_`")
+	fmt.Println(" .:( ^   :: ^   ):. ")
+	fmt.Println(" `:::   (..)   :::. ")
+	fmt.Println(" `:::::::UU:::::::` ")
+	fmt.Println(" .::::::::::::::::. ")
+	fmt.Println(" O::::::::::::::::O ")
+	fmt.Println(" -::::::::::::::::- ")
+	fmt.Println(" `::::::::::::::::` ")
+	fmt.Println("  .::::::::::::::.  ")
+	fmt.Println("    oO:::::::Oo     ")
+}
